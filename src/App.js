67c8e8ed25d198481c1af71f816bc57cd5f4fe85
Noahkimtai <noahkimtai24@gmail.com>
2022-11-08 15:52:47 +0300

    Components put together

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import react from 'react'
-import { Switch, Route } from "react-router-dom"
+//import { Switch, Route } from "react-router-dom"
 import Navbar from './Components/Navbar';
 import SearchBar from './Components/SearchBar';
 import MusicCard  from './Components/MusicCard';
@@ -8,13 +8,8 @@ function App() {
 
     return (
         <div className='App'>
-        <Navbar/>
-        <Switch>
-            
-        </Switch>
-        </div>
-        <div>
             <Navbar />
+            <SearchBar />
             <MusicCard />
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import './index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
-=======
+import './App.css';
+//import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
->>>>>>> c51594e56216fb4e537d0bcf41a6d7f59e8542cf
+import App from './App.js';
 
-ReactDOM.render(
-<BrowserRouter>
-  <App /> 
-</BrowserRouter>,
-document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>
+);
