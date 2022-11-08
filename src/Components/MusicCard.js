d68328da8b05c2d39de8eaea27d5d2bf826e5aea
Noahkimtai <noahkimtai24@gmail.com>
2022-11-08 19:17:08 +0300

    style changed and evnent listener for rate and add to favorite added

--- src/Components/MusicCard.js
+++ src/Components/MusicCard.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 import MusicDetails from './MusicDetails'
-
+  
 function MusicCard(){
+/*     const [songs, setSongs] = useState([]);
+    const [favorite, setFavorite] = useState([])
+    const [count, setCount] = useState(0)
+    useEffect(()=>{
+        fetch('enter backend api').then(res => res.json())
+        .then(data =>setProducts(data));
+    },[]) */
+    
+    function addToFavorite(id){
+/*         const filteredData = songs.filter(el =>el.id ===id)
+        setFavorite((favorite) => favorite.concat(filteredData))
+        setCount((count) =>count +1) */
+        alert('clicky')
+    }
+
     // for every song item returned from the api the music details component should be loaded
+    
     return(
-        <MusicDetails />  
+        <div>
+            <MusicDetails addToFavorite ={addToFavorite}/>
+        </div> 
     ); 
 }
 export default MusicCard;
