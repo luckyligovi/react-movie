278e3fadc83b1f73fdde1d12bb9088ece1b8602c
KipyegonB <koechbernard08@gmail.com>
2022-11-08 14:22:22 +0300

    First commit implementing search components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import react from 'react'
+import { Switch, Route } from "react-router-dom"
 import Navbar from './Components/Navbar';
+import SearchBar from './Components/SearchBar';
 
 function App() {
 
     return (
+        <div className='App'>
         <Navbar/>
+        <Switch>
+            
+        </Switch>
+        </div>
     )
 }
 
