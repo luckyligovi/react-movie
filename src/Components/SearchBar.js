bf97b1cd6b7ad942cd989080c846a46823f57205
KipyegonB <koechbernard08@gmail.com>
2022-11-09 14:28:58 +0300

    update deatils on SearchBar and Navbar

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,17 +1,37 @@
-import React from "react";
-  
-function SearchBar({ searchInput, search }) {
+import { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { BiSearch } from 'react-icons/bi';
+
+const SearchBar = () => {
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`);
+    setSearch('');
+  };
+
   return (
-    <div className="search-bar">
-      <input
-        type="text"
-        placeholder="Search for a song..."
-        className="search"
-        onChange={searchInput}
-        onKeyPress={search}
-      />
+    <div>
+      <nav id="searchbar">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter song name..."
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          />
+          <button type="submit">
+            <BiSearch />
+          </button>
+        </form>
+      </nav>
     </div>
   );
-}
-  
+};
+
 export default SearchBar;
