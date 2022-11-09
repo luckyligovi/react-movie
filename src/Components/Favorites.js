b2653bde75c478fd2b2c17dd8c6abce8ebd88d4e
Lucky Ligovi <luckyligovi@gmail.com>
2022-11-09 09:15:27 +0300

    favorites componet features upadate

--- src/Components/Favorites.js
+++ src/Components/Favorites.js
@@ -1,9 +1,16 @@
 import React from "react";
 
 function Favorites() {
+    const song = {
+        image : "https://pictures-kenya.jijistatic.com/17577705_20210225-153359_1500x1125.webp",
+        name : "Phocey"
+    }
+
     return (
     <div>
         <h1>Favorite music</h1>
+        <p>{song.name}</p>
+        <img src={song.image} alt="phoncey album" />
     </div>
     );
   }
